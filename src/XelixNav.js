84b4c678efecce8a6cbf54a4bcc0bcec51c72f51
Viktoria <96484999+viki3612@users.noreply.github.com>
2024-06-17 05:22:16 -0700

    Update XelixNav.js

--- src/XelixNav.js
+++ src/XelixNav.js
@@ -13,10 +13,10 @@ export default function Navigation () {
  <img src={logo} alt="logo" width="130" height="90" className="img-fluid">
       </img>
       <ul className="navbar-nav ms-auto mb-2">
-      <img src={avatar} alt="profile-image" width="40" height="42"  className="profile-image">
+      <img src={avatar} alt="avatar"width="40" height="42"  href="#" className="profile-image">
       </img>
         <li className="nav-item">
-          <a className="nav-link" aria-current="page">Home</a>
+          <a className="nav-link" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item-2">
           <a className="nav-link">Dashboard</a>

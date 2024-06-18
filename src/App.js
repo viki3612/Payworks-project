ac2f785f76a928095d36839316a842e1fc489be0
Viktoria <96484999+viki3612@users.noreply.github.com>
2024-06-18 14:28:59 -0700

    small edit to href attributes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import React from "react";
 import XelixNav from "./XelixNav";
-
+import styles from "./styles.css";
 export default function App() {
   
   return (
     <div className="App">
         <XelixNav />
-     
+        {styles}
     </div>
   );
 }
